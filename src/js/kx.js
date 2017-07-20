9762292f65cf6dca2a8c94b0ddb27d7544d6b9c3
hukai <704266819@qq.com>
2017-07-20 22:07:23 +0800

    first commit

---
+++ src/js/kx.js
@@ -0,0 +1,21 @@
+$(function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: 3000,
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    })
+
+	// function selectOnchang(obj){ 
+	// 	//获取被选中的option标签选项 
+	// 	alert(obj.selectedIndex);
+	// }  
+	$('.gnjd').mouseover(function(){
+		$('.jfdt').css("background-image","url(images/guoneijf.png)");
+	});
+	$('.gwjd').mouseover(function(){
+		$('.jfdt').css("background-image","url(images/guowaijf.png)");
+	})
+
+})
