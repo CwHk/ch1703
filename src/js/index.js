f0a2a04303f4b117a79a2daa46e2e84f8ee488a5
hukai <704266819@qq.com>
2017-08-08 17:18:40 +0800

    删除了老版首页的地图的样式和事件

--- src/js/index.js
+++ src/js/index.js
@@ -11,12 +11,6 @@ $(function() {
     // 	//获取被选中的option标签选项 
     // 	alert(obj.selectedIndex);
     // }  
-    // $('.gnjd').mouseover(function() {
-    //     $('.jfdt').css("background-image", "url(images/guoneijf.png)");
-    // });
-    $('.gwjd').mouseover(function() {
-        $('.jfdt').css("background-image", "url(images/guowaijf.png)");
-    });
     $('.tab-hot').on('click','.tab-item',function(){
         $(this).addClass('active').siblings().removeClass('active');
     })
